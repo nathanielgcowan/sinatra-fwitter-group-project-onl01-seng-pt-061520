16f70b57407d140906364521459622b33adeafd5
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-29 03:55:05 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :Sessions
+    enable :sessions
     set :session_secret, "fwitter_secret"
   end
 
