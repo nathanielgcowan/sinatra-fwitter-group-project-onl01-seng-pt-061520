fb1029c3202982217568881c54f5577fd8e768c1
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-29 00:14:13 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable 'Sessions'
   end
 
+  helpers do
+
+
+  end
 end
