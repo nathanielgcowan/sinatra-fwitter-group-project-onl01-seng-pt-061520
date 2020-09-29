73b82fbef1964f9fb386a6b417535940a2ecfbf4
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-29 03:04:03 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_09_28_212505) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.text "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
