26ac06ffe2cb3e24a119ecf2e4adf5e3c349d922
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-29 05:10:07 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,4 @@
 class TweetsController < ApplicationController
-
   get '/tweets' do
     if logged_in?
       @tweets = Tweet.all
@@ -45,7 +44,6 @@ class TweetsController < ApplicationController
 
   get '/tweets/:id/edit' do
     if logged_in?
-      ##couple things to do
       @tweet = Tweet.find_by_id(params[:id])
       if @tweet && @tweet.user == current_user
         erb :'tweets/edit_tweet'
@@ -58,9 +56,35 @@ class TweetsController < ApplicationController
   end
 
   patch '/tweets/:id' do
-
-
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/tweets/#{params[:id]}/edit"
+      else
+        @tweet = Tweet.find_by_id(params[:id])
+        if @tweet && @tweet.user == current_user
+          if @tweet.update(content: params[:content])
+            redirect to "/tweets/#{@tweet.id}"
+          else
+            redirect to "/tweets/#{@tweet.id}/edit"
+          end
+        else
+          redirect to '/tweets'
+        end
+      end
+    else
+      redirect to '/login'
+    end
   end
 
-
+  delete '/tweets/:id/delete' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      if @tweet && @tweet.user == current_user
+        @tweet.delete
+      end
+      redirect to '/tweets'
+    else
+      redirect to '/login'
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
       session.destroy
       redirect to '/login'
     else
-      redirect to '/tweets'
+      redirect to '/'
     end
   end
 end
